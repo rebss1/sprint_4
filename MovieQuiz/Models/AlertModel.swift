b763fb7928394bf29ee7f49f8699446061c02c4f
loshilov ilya <loshilovilya@yandex.ru>
2023-11-14 12:17:22 +0300

    добавил два файла для алерта

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,14 @@
+//
+//  AlertModel.swift
+//  MovieQuiz
+//
+//  Created by Илья Лощилов on 14.11.2023.
+//
+
+import Foundation
+
+struct AlertModel {
+    let title: String
+    let message: String
+    let buttonText: String
+}
---
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  AlertPresenter.swift
+//  MovieQuiz
+//
+//  Created by Илья Лощилов on 14.11.2023.
+//
+
+import Foundation
